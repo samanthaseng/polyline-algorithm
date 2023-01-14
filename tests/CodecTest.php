619da45a3b892bd4073779eb043bb908d1681be8
Samantha Seng <samantha.seng@airweb.fr>
2023-01-14 14:47:12 +0100

    :test_tube: Add encoding unit tests

--- tests/CodecTest.php
+++ tests/CodecTest.php
@@ -7,24 +7,104 @@ use SamanthaSeng\PolylineAlgorithm\Codec;
 
 class CodecTest extends TestCase
 {
-	/**
-	 * @test
-	 *
-	 * @return void
-	 */
+	const PATH_CASES = [
+		'POSITIVE' => [
+			[38.5, 120.2],
+			[40.7, 120.95],
+			[43.252, 126.453],
+		],
+		'NEGATIVE' => [
+			[-38.5, -120.2],
+			[-40.7, -120.95],
+			[-43.252, -126.453],
+		],
+		'MIXED' => [
+			[38.5, -120.2],
+			[40.7, -120.95],
+			[43.252, -126.453],
+		],
+		'ROUNDED' => [
+			[39, -120],
+			[41, -121],
+			[43, -126],
+		],
+		'EMPTY' => [],
+	];
+
+	public function testCanEncodePositiveCoordinateArray(): void
+	{
+		$codec = new Codec();
+		$encodedValue = $codec->encode(self::PATH_CASES['POSITIVE']);
+
+		$this->assertEquals('string', gettype($encodedValue));
+		$this->assertEquals('_p~iF_qs|U_ulLonqC_mqNwxq`@', $encodedValue);
+	}
+
+	public function testCanEncodeNegativeCoordinateArray(): void
+	{
+		$codec = new Codec();
+		$encodedValue = $codec->encode(self::PATH_CASES['NEGATIVE']);
+
+		$this->assertEquals('string', gettype($encodedValue));
+		$this->assertEquals('~o~iF~ps|U~tlLnnqC~lqNvxq`@', $encodedValue);
+	}
+
+	public function testCanEncodeMixedCoordinateArray(): void
+	{
+		$codec = new Codec();
+		$encodedValue = $codec->encode(self::PATH_CASES['MIXED']);
+
+		$this->assertEquals('string', gettype($encodedValue));
+		$this->assertEquals('_p~iF~ps|U_ulLnnqC_mqNvxq`@', $encodedValue);
+	}
+
+	public function testCanEncodeRoundedCoordinateArray(): void
+	{
+		$codec = new Codec();
+		$encodedValue = $codec->encode(self::PATH_CASES['ROUNDED']);
+
+		$this->assertEquals('string', gettype($encodedValue));
+		$this->assertEquals('_e`mF~nl{U_seK~hbE_seK~po]', $encodedValue);
+	}
+
+	public function testCanEncodeObjectCoordinateArray(): void
+	{
+		$objectArray = array_map(function ($array) {
+			return (object) [
+				'lat' => $array[0],
+				'lng' => $array[1],
+			];
+		}, self::PATH_CASES['MIXED']);
+
+		$codec = new Codec();
+		$encodedValue = $codec->encode($objectArray);
+
+		$this->assertEquals('_p~iF~ps|U_ulLnnqC_mqNvxq`@', $encodedValue);
+	}
+
 	public function testCanEncodeEmptyArray(): void
 	{
 		$codec = new Codec();
+		$encodedValue = $codec->encode(self::PATH_CASES['EMPTY']);
 
-		$this->assertEquals('', $codec->encode([]));
+		$this->assertEquals('', $encodedValue);
 	}
 
 	public function testCanEncodeWithCustomPrecision(): void
 	{
 		$codec = new Codec();
-		$encodedValue = $codec->encode([[38.5, -120.2]], 6);
+		$encodedValue = $codec->encode(self::PATH_CASES['MIXED'], 6);
 
 		$this->assertEquals('string', gettype($encodedValue));
-		$this->assertEquals('_izlhA~rlgdF', $encodedValue);
+		$this->assertEquals('_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI', $encodedValue);
+	}
+
+	public function testCanEncodeWithZeroPrecision(): void
+	{
+		$codec = new Codec();
+		$encodedValue = $codec->encode(self::PATH_CASES['MIXED'], 0);
+
+		$this->assertEquals('string', gettype($encodedValue));
+		$this->assertEquals('kAnFC?EJ', $encodedValue);
 	}
 }
